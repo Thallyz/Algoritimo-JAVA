5287f8ae61ae6d8e3829345f15d6a3bbc21f56d1
Thallyz <youthman95@gmail.com>
2024-09-12 13:48:41 -0300

    Aula 12-set-2024

---
+++ Mercado/src/cliente.java
@@ -0,0 +1,28 @@
+public class cliente {
+    private String nome;
+    private String email;
+
+    public cliente(String nome, String email){
+        this.nome=nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @Override
+    public String toString(){
+        return "Cliente: "+ nome+"Email: "+email;
+    }
+}
