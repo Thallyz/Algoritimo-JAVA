0e22cfd1ebe58a97e1a0031c2b1022273425e07b
Thallyz <youthman95@gmail.com>
2024-09-17 16:16:11 -0300

    Aula 12-set-2024

--- Bibilhoteca/src/Livro.java
+++ Bibilhoteca/src/Livro.java
@@ -1,2 +1,33 @@
 public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro(int id,String titulo, String autor, boolean disponivel){
+        this.id=id;
+        this.autor=autor;
+        this.titulo=titulo;
+        this.disponivel=true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public static Livro estaDisponivel() {
+        return estaDisponivel();
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Algoritimo-JAVA
 Nome: Thallyson Glória Alves
-Matrícula: 2023101100100486
-test2
+Matrícula: 202310110010048
