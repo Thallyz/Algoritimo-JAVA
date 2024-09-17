a943c3c9a4e1f1cec1e8c2c279f706e9c378c77e
Thallyz <youthman95@gmail.com>
2024-09-17 17:12:41 -0300

    Aula 12-set-2024

--- Bibilhoteca/src/Usuario.java
+++ Bibilhoteca/src/Usuario.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Usuario {
+    private int id;
+    private String nome;
+    private ArrayList<Livro> livroAdiquirido;
+
+    public Usuario(int id, String nome, ArrayList<Livro> livroAdiquirido) {
+        this.id = id;
+        this.nome = nome;
+        this.livroAdiquirido = livroAdiquirido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public ArrayList<Livro> getLivroAdiquirido() {
+        return livroAdiquirido;
+    }
+    public void addLivro(Livro livro){
+        livroAdiquirido.add(livro);
+    }
+    public void removeLivro(Livro livro){
+        livroAdiquirido.remove(livro);
+    }
 }
