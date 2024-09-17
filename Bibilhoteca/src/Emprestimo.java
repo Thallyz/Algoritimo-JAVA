0bbb615cbd449398c20a55c0e761f996f161df73
Thallyz <youthman95@gmail.com>
2024-09-17 17:22:23 -0300

    Aula 12-set-2024

---
+++ Bibilhoteca/src/Emprestimo.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+
+public class Emprestimo {
+    private Livro livro;
+    private Usuario usuario;
+    private LocalDate dateEmprestimo;
+    private LocalDate dataDevolucao;
+
+    public Emprestimo(Livro livro, Usuario usuario, LocalDate dateEmprestimo, LocalDate dataDevolucao) {
+        this.livro = livro;
+        this.usuario = usuario;
+        this.dateEmprestimo = dateEmprestimo.now();
+        this.dataDevolucao = dataDevolucao.plusDays(7);
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public LocalDate getDateEmprestimo() {
+        return dateEmprestimo;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+}
