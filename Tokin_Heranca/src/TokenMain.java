997aa51c3323cdbc8405a313ae9bf0aac4ef9ff8
Thallyz <youthman95@gmail.com>
2024-10-18 16:15:11 -0300

    TokenMain 10-10-2024

--- Tokin_Heranca/src/TokenMain.java
+++ Tokin_Heranca/src/TokenMain.java
@@ -5,5 +5,19 @@ public class TokenMain {
 
     TokenNaoFungivel tokenB= new TokenNaoFungivel("Token B",1500.00, "987654321123456789");
     tokenB.mostraDetalhes();
+        System.out.println("\n");
+
+        //outros tipos de consutar os dados.
+
+        System.out.println(tokenA.getClass());
+        System.out.println(tokenA.getNome());
+        System.out.println(tokenA.getValor());
+        System.out.println(tokenA.getQtd()+"\n");
+
+        System.out.println(tokenB.getClass());
+        System.out.println(tokenB.getNome());
+        System.out.println(tokenB.getValor());
+        System.out.println(tokenB.getIdUnico());
+
     }
 }
